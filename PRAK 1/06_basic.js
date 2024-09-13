d2624034bc3fbc38806aba85cd07e305cb34128e
Ivan Yugava <117693988+VanYewgava@users.noreply.github.com>
2024-09-13 14:47:29 +0700

    Add files via upload
    Praktikum 1

---
+++ PRAK 1/06_basic.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <tittle>Belajar Javascript</tittle>
+    </head>
+    <body>
+        <h1>Looping: while</h1>
+        <script>
+        counter=0;
+        while (counter < 5){
+            document.write("Counter: " + counter + "<br />");
+            counter++;
+        }
+        </script>
+    </body>
+</html>
